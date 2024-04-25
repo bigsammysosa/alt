bed6ff291c694c912cae55bff4ccd8be695ed1c3
bigsammysosa <154764625+bigsammysosa@users.noreply.github.com>
2024-04-25 08:30:54 +0200

    Add files via upload

---
+++ alt/assets/js/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * toggle navbar
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+const btn = document.getElementById('getS');
+
+btn.addEventListener('click', () => {
+	
+	alert('Are you looking to promote your brand?');
+	
+	
+			 <a  id="yes" class="btn btn-primary">Yes, Get Started</a>
+			  <a  id="no" class="btn btn-primary">No, Im an Advertiser</a>
+			  
+			  const btn = document.getElementById('getS');
+			  const btn = document.getElementById('getS');
+			  
+			  
+			if() {}
+	
+} );
+
